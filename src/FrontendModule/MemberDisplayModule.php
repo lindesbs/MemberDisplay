32f641153ee84a8f00f8d5eeb5b6bbc7582acdde
Stefan Lindecke <lindecke@odas.biz>
2021-10-04 17:22:14 +0200

    Updates

--- src/FrontendModule/MemberDisplayModule.php
+++ src/FrontendModule/MemberDisplayModule.php
@@ -4,68 +4,72 @@
 namespace lindesbs\MemberDisplay\FrontendModule;
 
 use Contao\BackendTemplate;
-use Contao\CoreBundle\Image\Studio;
-
-use Contao\CoreBundle\Image\Studio\LegacyFigureBuilderTrait;
+use Contao\FilesModel;
 use Contao\Module;
+use Contao\StringUtil;
 use Contao\System;
 
 class MemberDisplayModule extends Module
 {
-    use LegacyFigureBuilderTrait;
 
-    /**
-     * Template
-     *
-     * @var string
-     */
-    protected $strTemplate = 'mod_MemberDisplay';
-
-
-    /**
-     * Display a wildcard in the back end
-     *
-     * @return string
-     */
-    public function generate()
-    {
-        $request = System::getContainer()->get('request_stack')->getCurrentRequest();
-
-        if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request)) {
-            $objTemplate = new BackendTemplate('be_wildcard');
-            $objTemplate->wildcard = '### MemberDisplay ###';
-            $objTemplate->title = $this->headline;
-            $objTemplate->id = $this->id;
-            $objTemplate->link = $this->name;
-            $objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
-
-            return $objTemplate->parse();
-        }
+	/**
+	 * Template
+	 *
+	 * @var string
+	 */
+	protected $strTemplate = 'mod_MemberDisplay';
 
 
+	/**
+	 * Display a wildcard in the back end
+	 *
+	 * @return string
+	 */
+	public function generate()
+	{
+		$request = System::getContainer()->get('request_stack')->getCurrentRequest();
 
-        return parent::generate();
-    }
+		if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
+		{
+			$objTemplate = new BackendTemplate('be_wildcard');
+			$objTemplate->wildcard = '### MemberDisplay ###';
+			$objTemplate->title = $this->headline;
+			$objTemplate->id = $this->id;
+			$objTemplate->link = $this->name;
+			$objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
 
-    /**
-     * Generate the module
-     */
-    protected function compile()
-    {
-        $mitarbeiter = $this->Database->prepare("SELECT * FROM tl_member WHERE id=?")->limit(1)->execute($this->mitarbeiter);
-
-        $this->Template->mitarbeiter =$mitarbeiter;
-
-        $figureBuilder = $this->getFigureBuilderIfResourceExists($mitarbeiter->primaryimage);
-        if ($figureBuilder !== null) {
-            $this->Template->primaryImage = new \stdClass();
-            $figureBuilder->setSize($this->size)
-                ->build()
-                ->applyLegacyTemplateData($this->Template->primaryImage, $this->imagemargin, $this->floating);
+			return $objTemplate->parse();
+		}
 
 
-        }
-    }
+		return parent::generate();
+	}
+
+	/**
+	 * Generate the module
+	 */
+	protected function compile()
+	{
+		$container = System::getContainer();
+		$rootDir = $container->getParameter('kernel.project_dir');
+
+
+		$mitarbeiter = $this->Database->prepare("SELECT * FROM tl_member WHERE id=?")->limit(1)->execute($this->mitarbeiter);
+
+		$this->Template->mitarbeiter = $mitarbeiter;
+
+		$objFile = FilesModel::findByUuid($mitarbeiter->primaryimage);
+		$path = $objFile->path;
+		if ($objFile !== null || is_file($rootDir . '/' . $path))
+		{
+			$picture = $container
+				->get('contao.image.picture_factory')
+				->create($rootDir . '/' . $path, StringUtil::deserialize($this->size));
+
+			$this->Template->primaryImage = $picture->getImg($rootDir);
+
+		}
+	}
 }
 
 class_alias(MemberDisplayModule::class, 'MemberDisplayModule');
